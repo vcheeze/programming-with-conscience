38346e4fda473d0a1ffc261b0d32caadf87da769
Peter Chen <manchesterutd7@gmail.com>
2025-11-25 17:03:48 +0400

    feat: add moment article

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -44,7 +44,7 @@ const CardDecorator = ({ children }: { children: ReactNode }) => (
   <div className="mask-radial-from-40% mask-radial-to-60% relative mx-auto size-36 duration-200 [--color-border:color-mix(in_oklab,var(--color-foreground)10%,transparent)] group-hover:[--color-border:color-mix(in_oklab,var(--color-foreground)20%,transparent)] dark:[--color-border:color-mix(in_oklab,var(--color-white)15%,transparent)] dark:group-hover:[--color-border:color-mix(in_oklab,var(--color-white)20%,transparent)]">
     <div
       aria-hidden
-      className="absolute inset-0 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px] dark:opacity-50"
+      className="absolute inset-0 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-size-[27px_27px] dark:opacity-50"
     />
 
     <div className="bg-background absolute inset-0 m-auto flex size-12 items-center justify-center border-l border-t">
